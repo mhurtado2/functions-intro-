ff6c23e4057786d54f281bc88e14ca51114921f6
Shane <mhurtado1@berklee.edu>
2022-12-12 09:37:15 -0600

    func-intro

---
+++ main.js
@@ -0,0 +1,12 @@
+const fillGasTank = (gallons) => {
+    //for (const gallon of gallons)
+    if (gallons > 15) {
+        console.log("You cannot add this much gas to the tank")
+    }
+    else {
+        console.log(`I filled the tank with ${gallons} of gas`)
+    }
+
+}
+fillGasTank(15)
+fillGasTank(30)
